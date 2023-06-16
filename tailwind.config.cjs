9e7b8ee5bb43a8d54460249c918c60dca6d6eada
JudLup Luna <judlup@gmail.com>
2023-06-16 03:09:33 -0500

    Se redimensionaron los espacios en el header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'tc-black': '#232122',
+				'tc-cyan': '#A2F2F2',
+				'tc-yellow': '#EDF2C2',
+				'tc-gray': '#B5B5B5',
+				'tc-gray-l': '#F2F2F2',
+			}
+		},
 	},
 	plugins: [],
 }
